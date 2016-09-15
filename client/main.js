b41303f97e89887d28d53f638d936c3397f731c6
Anatoliy Borshch <anatoliy.borshch@playtech.com>
2016-09-16 00:49:28 +0300

    step-7

--- client/main.js
+++ client/main.js
@@ -1,3 +1,14 @@
 Template.stream.posts = function () {
     return Posts.find({}, {sort: {published: -1}});
 };
+
+Template.editor.events({
+    "click #submit-post": function() {
+        var published_date = new Date();
+        Posts.insert({
+            title: document.getElementById("title-area").value,
+            text: document.getElementById("editor-area").value,
+            published: published_date.toLocaleTimeString()
+        })
+    }
+});
